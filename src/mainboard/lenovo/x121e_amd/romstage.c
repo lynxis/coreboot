54f2fb732febaf6fd01bba345230d049726367de
Alexander Couzens <lynxis@fe80.eu>
2015-02-14 16:22:38 +0100

    x121e: removed nuvoton EC
    Change-Id: I8e419f3ed7be6a6278ab974cee7665556dc1af74

--- src/mainboard/lenovo/x121e_amd/romstage.c
+++ src/mainboard/lenovo/x121e_amd/romstage.c
@@ -62,7 +62,6 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 		sb_Poweron_Init();
 
 		post_code(0x31);
-		nuvoton_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 		console_init();
 	}
 
